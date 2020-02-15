7dfb63e07a16fa6d9bdadb1d7081801631d0b155
Morgan Sommer <morgana.sommer@gmail.com>
2020-02-15 13:24:35 -0500

    database schema, server file, and seed file created

---
+++ sql/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(15.2),
+  department_id INT,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY(id)
+);
